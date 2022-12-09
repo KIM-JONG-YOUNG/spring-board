3aa757b0e840a041419df6f44e95738155a34dba
kim-jong-young <kim-jong-young@218.38.137.27>
2022-12-10 00:17:29 +0900

    role domain test 수정

--- spring-board-domains/spring-board-role-domain/src/test/java/edu/jong/board/member/test/RoleDomainTests.java
+++ spring-board-domains/spring-board-role-domain/src/test/java/edu/jong/board/member/test/RoleDomainTests.java
@@ -3,14 +3,16 @@ package edu.jong.board.member.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import edu.jong.board.common.BoardConstants;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootTest
+@ActiveProfiles("test")
 @SpringBootApplication(scanBasePackages = BoardConstants.ROOT_PACKAGE)
-public class SpringBoardRoleDomainTests {
+public class RoleDomainTests {
 
 	@Test
 	void contextLoad() {
